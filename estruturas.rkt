30a56090d928452ebda42458563b2b29ad9efdd4
Lucas Rohr Carreno <lucasrc17@live.com>
2022-07-07 21:02:09 -0300

    feat: :sparkles: adding structure exercises

--- estruturas.rkt
+++ estruturas.rkt
@@ -85,7 +85,7 @@
 ;;       #false
 ;;     )
 ;;   )
-;; )
+;; ) = "Jorge"
 
 (define (retorna-nome-aluno aluno)
   (aluno-nome aluno)
@@ -113,7 +113,7 @@
 ;; Objetivo: dada uma estrutura prova, devolve a soma de suas notas
 
 ;; Exemplo:
-;; (retorna-total-prova (make-prova 5 5) (make-prova 5 5))
+;; (retorna-total-prova (make-prova 5 5)) = 10
 
 (define (retorna-total-prova prova)
   (+ (prova-nota-1 prova) (prova-nota-2 prova))
@@ -141,7 +141,7 @@
 ;;              #false
 ;;          )
 ;;       )
-;;)
+;;) = 8.5
 
 (define (retorna-media-provas aluno)
   (/
@@ -168,3 +168,477 @@
   8.5
 )
 
+;; (d) Dados 2 alunos, devolve o aluno com melhor média aritmética das provas.
+
+;;retorna-aluno-melhor-media: aluno, aluno -> aluno
+;; Objetivo: dado dois alunos, calcula a media aritmetica de cada um e retorna aquele aluno com a maior media
+
+;; Exemplo:
+;; (retorna-media-provas
+;;       (make-aluno
+;;          "Jorge"
+;;          (make-prova 5 4)
+;;          (make-prova 4 3)
+;;          (make-exercicios
+;;             (make-nota-exercicio 8 7)
+;;             (make-nota-exercicio 8 7)
+;;              #false
+;;          )
+;;       )
+;;       (make-aluno
+;;          "Jorge"
+;;          (make-prova 5 4)
+;;          (make-prova 4 3)
+;;          (make-exercicios
+;;             (make-nota-exercicio 8 7)
+;;             (make-nota-exercicio 8 7)
+;;              #false
+;;          )
+;;       )
+;;) =
+;;  (make-aluno
+;;     "Miguel"
+;;     (make-prova 5 5)
+;;     (make-prova 4 5)
+;;     (make-exercicios
+;;        (make-nota-exercicio 8 7)
+;;        (make-nota-exercicio 8 7)
+;;        #false
+;;     )
+;;  )
+
+(define (retorna-aluno-melhor-media aluno-1 aluno-2)
+  (cond
+     [(>= (retorna-media-provas aluno-1) (retorna-media-provas aluno-2)) aluno-1]
+     [else aluno-2]
+   )
+)
+
+;; Testes:
+
+(check-expect
+  (retorna-aluno-melhor-media
+       (make-aluno
+          "Jorge"
+          (make-prova 5 4)
+          (make-prova 3 2)
+          (make-exercicios
+             (make-nota-exercicio 8 7)
+             (make-nota-exercicio 8 7)
+              #false
+          )
+       )
+       (make-aluno
+          "Miguel"
+          (make-prova 5 5)
+          (make-prova 4 5)
+          (make-exercicios
+             (make-nota-exercicio 8 7)
+             (make-nota-exercicio 8 7)
+              #false
+          )
+       )
+  )
+  (make-aluno
+     "Miguel"
+     (make-prova 5 5)
+     (make-prova 4 5)
+     (make-exercicios
+        (make-nota-exercicio 8 7)
+        (make-nota-exercicio 8 7)
+        #false
+     )
+  )
+)
+
+
+;; (e) Dados 2 alunos, devolve o nome do aluno com melhor média das provas.
+
+;;retorna-nome-aluno-melhor-media: aluno, aluno -> String
+;; Objetivo: dado dois alunos, calcula a media aritmetica de cada um e retorna o nome daquele aluno com a maior media
+
+;; Exemplo:
+;; (retorna-nome-aluno-melhor-media
+;;       (make-aluno
+;;          "Jorge"
+;;          (make-prova 5 4)
+;;          (make-prova 4 3)
+;;          (make-exercicios
+;;             (make-nota-exercicio 8 7)
+;;             (make-nota-exercicio 8 7)
+;;              #false
+;;          )
+;;       )
+;;       (make-aluno
+;;          "Miguel"
+;;          (make-prova 5 5)
+;;          (make-prova 4 5)
+;;          (make-exercicios
+;;             (make-nota-exercicio 8 7)
+;;             (make-nota-exercicio 8 7)
+;;              #false
+;;          )
+;;       )
+;;) = "Miguel"
+
+(define (retorna-nome-aluno-melhor-media aluno-1 aluno-2)
+  (aluno-nome (retorna-aluno-melhor-media aluno-1 aluno-2))
+)
+
+(check-expect
+  (retorna-nome-aluno-melhor-media
+       (make-aluno
+          "Jorge"
+          (make-prova 5 4)
+          (make-prova 3 2)
+          (make-exercicios
+             (make-nota-exercicio 8 7)
+             (make-nota-exercicio 8 7)
+              #false
+          )
+       )
+       (make-aluno
+          "Miguel"
+          (make-prova 5 5)
+          (make-prova 4 5)
+          (make-exercicios
+             (make-nota-exercicio 8 7)
+             (make-nota-exercicio 8 7)
+              #false
+          )
+       )
+  )
+  "Miguel"
+)
+
+;; (f) Dado um registro de exercício, verifica se o aluno deve ganhar ponto extra: se tiver nota de
+;; documentação maior ou igual a 3, nos 2 exercícios, ganha. O resultado deve ser o registro atualizado.
+
+
+;; verifica-ponto-extra: exercicios -> Boolean
+;; Objetivo: dado um registro de exercicios, verifica se o aluno possui ponto extra ou não e retorna o valor lógico
+
+;; Exemplo:
+;; (verifica-ponto-extra
+;;   (make-exercicios(
+;;     (make-nota-exercicio 8 7)
+;;     (make-nota-exercicio 8 7)
+;;     #false
+;;   ))
+;; ) = #true
+
+(define (verifica-ponto-extra registro-exercicios)
+   (and
+      (>= (nota-exercicio-nota-doc (exercicios-nota-exercicio-1 registro-exercicios)) 3)
+      (>= (nota-exercicio-nota-doc (exercicios-nota-exercicio-2 registro-exercicios)) 3)
+   )
+)
+
+;; Testes:
+
+(check-expect
+   (verifica-ponto-extra
+     (make-exercicios
+       (make-nota-exercicio 4 4)
+       (make-nota-exercicio 4 5)
+       #false
+     )
+   )
+   #true
+)
+
+(check-expect
+   (verifica-ponto-extra
+     (make-exercicios
+       (make-nota-exercicio 4 2)
+       (make-nota-exercicio 4 5)
+       #false
+     )
+   )
+   #false
+)
+
+
+;; atualiza-ponto-extra: exercicios -> exercicios
+;; Objetivo: dado um registro de exercicios, verifica se o aluno possui ponto extra ou não e retorna o registrado atualizado
+
+;; Exemplo:
+;; (atualiza-ponto-extra
+;;   (make-exercicios(
+;;     (make-nota-exercicio 8 7)
+;;     (make-nota-exercicio 8 7)
+;;     #false
+;;   ))
+;;) =
+;;   (make-exercicios
+;;     (make-nota-exercicio 4 4)
+;;     (make-nota-exercicio 4 5)
+;;     #true
+;;   ) 
+
+(define (atualiza-ponto-extra registro-exercicios)
+  (make-exercicios
+     (exercicios-nota-exercicio-1 registro-exercicios)
+     (exercicios-nota-exercicio-2 registro-exercicios)
+     (verifica-ponto-extra registro-exercicios)
+  )
+)
+
+;; Testes:
+
+(check-expect
+   (verifica-ponto-extra
+     (make-exercicios
+       (make-nota-exercicio 4 4)
+       (make-nota-exercicio 4 5)
+       #false
+     )
+   )
+   (make-exercicios
+     (make-nota-exercicio 4 4)
+     (make-nota-exercicio 4 5)
+     #true
+   ) 
+)
+
+(check-expect
+   (verifica-ponto-extra
+     (make-exercicios
+       (make-nota-exercicio 4 2)
+       (make-nota-exercicio 4 5)
+       #false
+     )
+   )
+   (make-exercicios
+     (make-nota-exercicio 4 2)
+     (make-nota-exercicio 4 5)
+     #false
+   )  
+)
+
+;; (g) Dado um aluno, registra se ele deve ganhar ponto extra ou não.
+
+;; atualiza-aluno-ponto-extra: aluno -> aluno
+;; Objetivo: dado um aluno, atualiza o registro se o mesmo possui ponto extra com base na suta de exercícios do tipo doc
+
+;; Exemplo:
+;; (atualiza-aluno-ponto-extra
+;;     (make-aluno
+;;        "Miguel"
+;;        (make-prova 5 5)
+;;        (make-prova 4 5)
+;;        (make-exercicios
+;;           (make-nota-exercicio 8 7)
+;;           (make-nota-exercicio 8 7)
+;;           #false
+;;        )
+;;     )
+;;) =
+;;  (make-aluno
+;;     "Miguel"
+;;     (make-prova 5 5)
+;;     (make-prova 4 5)
+;;     (make-exercicios
+;;        (make-nota-exercicio 4 5)
+;;        (make-nota-exercicio 4 4)
+;;        #true
+;;     )
+;;  )
+
+(define (atualiza-aluno-ponto-extra aluno)
+  (make-aluno
+     (aluno-nome aluno)
+     (aluno-prova-1 aluno)
+     (aluno-prova-2 aluno)
+     (atualiza-ponto-extra (aluno-exercicios aluno))
+  )
+)
+
+;; Testes:
+
+(check-expect
+  (atualiza-aluno-ponto-extra
+     (make-aluno
+        "Miguel"
+        (make-prova 5 5)
+        (make-prova 4 5)
+        (make-exercicios
+           (make-nota-exercicio 4 5)
+           (make-nota-exercicio 4 4)
+           #false
+        )
+     )
+  )
+  (make-aluno
+     "Miguel"
+     (make-prova 5 5)
+     (make-prova 4 5)
+     (make-exercicios
+        (make-nota-exercicio 4 5)
+        (make-nota-exercicio 4 4)
+        #true
+     )
+  )
+)
+
+(check-expect
+  (atualiza-aluno-ponto-extra
+     (make-aluno
+        "Jorge"
+        (make-prova 3 2)
+        (make-prova 4 5)
+        (make-exercicios
+           (make-nota-exercicio 3 2)
+           (make-nota-exercicio 4 4)
+           #false
+        )
+     )
+  )
+  (make-aluno
+     "Jorge"
+     (make-prova 3 2)
+     (make-prova 4 5)
+     (make-exercicios
+        (make-nota-exercicio 3 2)
+        (make-nota-exercicio 4 4)
+        #false
+     )
+  )
+)
+
+;; (h) Dado um aluno, calcula sua nota: a nota é a média ponderada da prova 1, com peso 4, prova 2, com peso 5,
+;; e a nota dos exercícios, com peso 1. A nota dos exercícios deve ser a soma das notas dos exercícios 1 e 2, mais o ponto extra,
+;; se for o caso. Para cada exercício, a nota é média aritmética das notas de codificação e documentação.
+
+;; Auxiliar 1
+
+;; retorna-nota-exercicio: nota-exercicio -> Numero
+;; Objetivo: dado um exercício, calcula a sua média aritmética e retorna o valor
+
+;; Exemplo:
+;; (retorna-nota-exercicio (make-nota-exercicio 3 2)) = 2.5
+
+(define (retorna-nota-exercicio nota-exercicio)
+  (/
+    (+ (nota-exercicio-nota-codigo nota-exercicio) (nota-exercicio-nota-doc nota-exercicio))
+    2
+  )
+)
+
+;; Teste:
+
+(check-expect
+  (retorna-nota-exercicio (make-nota-exercicio 3 2))
+  2.5
+)
+
+;; Auxiliar 2
+
+;; retorna-nota-registro-exercicios: exercicios -> Numero
+;; Objetivo: dado um registro de exercícios, soma a nota de dois exercícios e um ponto extra, caso exista
+
+;; Exemplo:
+;; (retorna-nota-registro-exercicios
+;;       (make-exercicios
+;;          (make-nota-exercicio 3 2)
+;;          (make-nota-exercicio 4 4)
+;;          #false
+;;       )
+;; ) = 6.5
+
+(define (retorna-nota-registro-exercicios exercicios)
+  (+
+    (retorna-nota-exercicio (exercicios-nota-exercicio-1 exercicios))
+    (retorna-nota-exercicio (exercicios-nota-exercicio-2 exercicios))
+    (cond
+      [(verifica-ponto-extra exercicios) 1]
+      [else 0]
+    )
+  )
+)
+
+;; Teste:
+
+(check-expect
+   (retorna-nota-registro-exercicios
+       (make-exercicios
+          (make-nota-exercicio 3 2)
+          (make-nota-exercicio 4 4)
+          #false
+       )
+   )
+   6.5
+)
+
+(check-expect
+   (retorna-nota-registro-exercicios
+       (make-exercicios
+          (make-nota-exercicio 3 3)
+          (make-nota-exercicio 4 4)
+          #true
+       )
+   )
+   8
+)
+
+;; retorna-nota-aluno: aluno -> Numero
+;; Objetivo: dado um aluno, calcula sua média ponderada usando a nota de suas provas e exercícios, retornando o valor
+
+;; Exemplo:
+;; (retorna-nota-aluno
+;;   (make-aluno
+;;     "Jorge"
+;;     (make-prova 3 2)
+;;     (make-prova 4 5)
+;;     (make-exercicios
+;;        (make-nota-exercicio 3 2)
+;;        (make-nota-exercicio 4 4)
+;;        #false
+;;     )
+;;   )
+;; ) = 2.5
+
+(define (retorna-nota-aluno aluno)
+  (/
+    (+
+      (* (retorna-total-prova (aluno-prova-1 aluno)) 4)
+      (* (retorna-total-prova (aluno-prova-2 aluno)) 5)
+      (retorna-nota-registro-exercicios (aluno-exercicios aluno))
+    )
+    10
+  )
+)
+
+;; Testes:
+
+(check-expect
+   (retorna-nota-aluno
+     (make-aluno
+        "Jorge"
+        (make-prova 3 2)
+        (make-prova 4 5)
+        (make-exercicios
+           (make-nota-exercicio 3 2)
+           (make-nota-exercicio 4 4)
+           #false
+        )
+      )
+   )
+   7.15
+)
+
+(check-expect
+   (retorna-nota-aluno
+     (make-aluno
+        "Miguel"
+        (make-prova 3 3)
+        (make-prova 4 5)
+        (make-exercicios
+           (make-nota-exercicio 3 3)
+           (make-nota-exercicio 4 5)
+           #true
+        )
+      )
+   )
+   7.75
+)
