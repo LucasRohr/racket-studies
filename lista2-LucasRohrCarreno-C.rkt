96db1530f375299063e80bd080b3744ffb91ffa2
Lucas Rohr Carreno <lucasrc17@live.com>
2022-08-02 21:56:24 -0300

    feat: :sparkles: finishing second work list activities

--- lista2-LucasRohrCarreno-C.rkt
+++ lista2-LucasRohrCarreno-C.rkt
@@ -17,8 +17,8 @@
 ;;   um     : Número, tile na pocisão um.
 ;;   dois   : Número, tile na pocisão dois.
 ;;   tres   : Número, tile na pocisão tres.
-;;   quatro   : Número, tile na pocisão quatro.
-;;   cinco   : Número, tile na pocisão cinco.
+;;   quatro : Número, tile na pocisão quatro.
+;;   cinco  : Número, tile na pocisão cinco.
 ;;   seis   : Número, tile na pocisão seis.
 ;;   sete   : Número, tile na pocisão sete.
 ;;   oito   : Número, tile na pocisão oito.
@@ -73,7 +73,7 @@
 
 ;; Exemplos:
 ;; (desenha-tile 2) = desenha quadrado com 2 dentro
-;; (desenha-tile 0)desenha quadrado com 0 dentro
+;; (desenha-tile 0) = desenha quadrado com 0 dentro
 (define (desenha-tile numero)
   (overlay
    (rectangle 20 20 "outline" "black")
@@ -105,18 +105,17 @@
        (desenha-tile (estado-oito estado))))]))
 
 
-;; desenha-lista-estados : ListaDeEstados -> Imagem
-;; Objetivo : dada uma ListaDeEstados, retorna um grid de estado um
-;; ao lado do outro
+;; desenha-lista-estados : ListasDeEstados -> Imagem
+;; Objetivo : dada uma ListasDeEstados, retorna um grid de estado um ao lado do outro
 
 ;; Exemplos:
 ;; (desenha-lista-estados LISTA-ESTADOS-1) = desenha grids da LISTA-ESTADOS-1 um ao lado do outro
 ;; (desenha-lista-estados LISTA-ESTADOS-2) = desenha grids da LISTA-ESTADOS-2 um ao lado do outro
 (define (desenha-lista-estados lista-estados)
   (cond
-    ;; se a lista lista-estados estiver vazia, então devolver uma imagem vazia
+    ;; Se a lista lista-estados estiver vazia, então devolver uma imagem vazia
     [(empty? lista-estados) empty-image]
-    ;; senão, retorna o desenho do primeiro estado ao lado do desenho do restantes dos estados
+    ;; Senão, retorna o desenho do primeiro estado ao lado do desenho do restante dos estados
     [else (beside
            (desenha-estado (first lista-estados))
            (rectangle 15 0 "outline" "white")
@@ -170,18 +169,18 @@
 ;; ========================================================================
 
 ;; está-em? : Número ListaDeNúmeros -> Booleano
-;; Objetivo : dado um número de uma ListaDeNúmeros, retorna se o número está na lista ou não
+;; Objetivo : dado um número e uma ListaDeNúmeros, retorna se o número está na lista ou não
 
 ;; Exemplos:
 ;; (está-em? 4 LISTA-MOVE-DIR) = #true
 ;; (está-em? 0 LISTA-MOVE-ESQ) = #false
 (define (está-em? numero lista-numeros)
   (cond
-    ;; se a lista lista-numeros estiver vazia, então devolver falso
+    ;; Se a lista lista-numeros estiver vazia, então devolver falso
     [(empty? lista-numeros) #false]
-    ;; se o primeiro valor da lista for igual ao número informado, retornar true
+    ;; Se o primeiro valor da lista for igual ao número informado, retornar true
     [(= numero (first lista-numeros)) #true]
-    ;; senão, retornar a execução da função com o restante da lista
+    ;; Senão, retornar número junto com o restante da lista
     [else (está-em? numero (rest lista-numeros))]))
 
 ;; Testes:
@@ -189,7 +188,7 @@
 (check-expect (está-em? 4 LISTA-MOVE-DIR) #true)
 (check-expect (está-em? 0 LISTA-MOVE-ESQ) #false)
 
-;; estado-é-igual? : Estado ListaEstadoDeNúmeros -> Booleano
+;; estado-é-igual? : Estado Estado -> Booleano
 ;; Objetivo : dados dois estados, verifica se são iguais, auxiliando estado-está-em?
 
 ;; Exemplos:
@@ -212,20 +211,20 @@
 (check-expect (estado-é-igual? ESTADO-1 ESTADO-1) #true)
 (check-expect (estado-é-igual? ESTADO-1 ESTADO-2) #false)
 
-;; estado-está-em? : Estado ListaDeEstados -> Booleano
-;; Objetivo : dado um estado e uma ListaDeEstados, retorna se o estado está na lista
-;; usando estado-está-em? para auxiliar
+;; estado-está-em? : Estado ListasDeEstados -> Booleano
+;; Objetivo : dado um estado e uma ListasDeEstados, retorna se o estado está na lista
+;; usando estado-é-igual? para auxiliar
 
 ;; Exemplos:
 ;; (estado-está-em? ESTADO-1 LISTA-ESTADOS-1) = #true
 ;; (estado-está-em? ESTADO-1 LISTA-ESTADOS-2) = #false
 (define (estado-está-em? estado lista-estados)
   (cond
-    ;; se a lista lista-estados estiver vazia, então devolver falso
+    ;; Se a lista lista-estados estiver vazia, então devolver falso
     [(empty? lista-estados) #false]
-    ;; se o primeiro estado for igual ao estado informado, retornar true
+    ;; Se o primeiro estado for igual ao estado informado, retornar true
     [(estado-é-igual? estado (first lista-estados)) #true]
-    ;; senão, retornar a execução da função passando o resto da lista
+    ;; Senão, retornar o estado junto com o restante da lista
     [else (estado-está-em? estado (rest lista-estados))]))
 
 ;; Testes:
@@ -249,7 +248,7 @@
 ;; 2. Estado
 
 ;; verifica-troca-tile-esq : Estado Número -> ResultadoTroca
-;; Objetivo : dado um estado e a posição do seu tile blank, retorna vazio se não for possível trocar a tila
+;; Objetivo : dado um estado e a posição do seu tile blank, retorna vazio se não for possível trocar a tile
 ;; ou o novo estado com tile movida caso possível, com posições para esquerda
 
 ;; Exemplos:
@@ -282,7 +281,7 @@
 (check-expect (verifica-troca-tile-esq ESTADO-2 4) (make-estado 1 4 2 0 3 5 6 7 8))
 
 ;; verifica-troca-tile-dir : Estado Número -> ResultadoTroca
-;; Objetivo : dado um estado e a posição do seu tile blank, retorna vazio se não for possível trocar a tila
+;; Objetivo : dado um estado e a posição do seu tile blank, retorna vazio se não for possível trocar a tile
 ;; ou o novo estado com tile movida caso possível, com posições para direita
 
 ;; Exemplos:
@@ -315,7 +314,7 @@
 (check-expect (verifica-troca-tile-dir ESTADO-2 4) (make-estado 1 4 2 3 5 0 6 7 8))
 
 ;; verifica-troca-tile-cima : Estado Número -> ResultadoTroca
-;; Objetivo : dado um estado e a posição do seu tile blank, retorna vazio se não for possível trocar a tila
+;; Objetivo : dado um estado e a posição do seu tile blank, retorna vazio se não for possível trocar a tile
 ;; ou o novo estado com tile movida caso possível, com posições para cima
 
 ;; Exemplos:
@@ -329,9 +328,9 @@
        (cond [(= posicao-blank 4) 0] [else (estado-um estado)])
        (cond [(= posicao-blank 5) 0] [else (estado-dois estado)])
 
-       (cond [(= posicao-blank 3) (estado-zero estado)] [(= posicao-blank (estado-seis estado)) 0] [else (estado-tres estado)])
-       (cond [(= posicao-blank 4) (estado-um estado)] [(= posicao-blank (estado-sete estado)) 0] [else (estado-quatro estado)])
-       (cond [(= posicao-blank 5) (estado-dois estado)] [(= posicao-blank (estado-oito estado)) 0] [else (estado-cinco estado)])
+       (cond [(= posicao-blank 3) (estado-zero estado)] [(= posicao-blank 6) 0] [else (estado-tres estado)])
+       (cond [(= posicao-blank 4) (estado-um estado)] [(= posicao-blank 7) 0] [else (estado-quatro estado)])
+       (cond [(= posicao-blank 5) (estado-dois estado)] [(= posicao-blank 8) 0] [else (estado-cinco estado)])
 
        (cond [(= posicao-blank 6) (estado-tres estado)] [else (estado-seis estado)])
        (cond [(= posicao-blank 7) (estado-quatro estado)] [else (estado-sete estado)])
@@ -348,7 +347,7 @@
 (check-expect (verifica-troca-tile-cima ESTADO-2 4) (make-estado 1 0 2 3 4 5 6 7 8))
 
 ;; verifica-troca-tile-baixo : Estado Número -> ResultadoTroca
-;; Objetivo : dado um estado e a posição do seu tile blank, retorna vazio se não for possível trocar a tila
+;; Objetivo : dado um estado e a posição do seu tile blank, retorna vazio se não for possível trocar a tile
 ;; ou o novo estado com tile movida caso possível, com posições para baixo
 
 ;; Exemplos:
@@ -432,12 +431,14 @@
 ;; (gera-sucessores ESTADO-2) = desenho de sucessores do ESTADO-2 lado a lado
 (define (gera-sucessores estado)
   (cond
+    ;; Se o estado não possui um tile blank, retorna uma imagem vazia
     [(not (está-em? 0 (gera-lista-tiles estado))) empty-image]
+    ;; Senão, verifica os sucessores do estado para cada direção de movimento do tile blank
     [else (desenha-lista-estados
            (cons (verifica-sucessor estado "esq")
+                 (cons (verifica-sucessor estado "baixo")
                  (cons (verifica-sucessor estado "dir")
-                 (cons (verifica-sucessor estado "cima")
-                 (cons (verifica-sucessor estado "baixo") empty)))))]))
+                 (cons (verifica-sucessor estado "cima") empty)))))]))
 
 ;; ========================================================================
 ;;                                 QUESTÃO 6
@@ -446,44 +447,220 @@
 ;; de cada um dos estados recebidos. A lista retornada não pode conter estados repetidos.
 ;; ========================================================================
 
+;; retorna-sucessores-lista-estados : ListasDeEstados -> ListasDeEstados
+;; Objetivo : dado uma ListasDeEstados, retorna uma nova ListasDeEstados montada recursivamente
+;; contento os sucessores de cada estado iterado na lista
+
+;; Exemplos:
+;; (retorna-sucessores-lista-estados LISTA-ESTADOS-1) =
+;; (list
+;;  '()
+;;  '()
+;;  (make-estado 4 5 8 1 1 2 7 0 1)
+;;  (make-estado 4 5 8 0 1 2 1 7 1)
+;;  (make-estado 1 4 2 0 3 5 6 7 8)
+;;  (make-estado 1 4 2 3 7 5 6 0 8)
+;;  (make-estado 1 4 2 3 5 0 6 7 8)
+;;  (make-estado 1 0 2 3 4 5 6 7 8))
+
+;; (retorna-sucessores-lista-estados LISTA-ESTADOS-2) =
+;; (list
+;;   '()
+;;   '()
+;;   (make-estado 4 5 8 1 1 2 7 0 1)
+;;   (make-estado 4 5 8 1 1 2 1 7 1)
+;;   (make-estado 1 4 2 0 3 5 6 7 8)
+;;   (make-estado 1 4 2 3 7 5 6 0 8)
+;;   (make-estado 1 4 2 3 5 0 6 7 8)
+;;   (make-estado 1 0 2 3 4 5 6 7 8))
+
 (define (retorna-sucessores-lista-estados lista-estados)
   (cond
+    ;; Se a lista-estados estiver vazia, retorna vazio
     [(empty? lista-estados) empty]
+    ;; Se o primeiro estado da lista não for vazio, verifica e retorna seus sucessores
+    ;; e monta em uma lista juntamente com os sucessores do restante da lista de estados
     [(not (empty? (first lista-estados)))
-     (cons (verifica-sucessor (first lista-estados) "dir")
-           (cons (verifica-sucessor (first lista-estados) "esq")
-           (cons (verifica-sucessor (first lista-estados) "cima")
+     (cons (verifica-sucessor (first lista-estados) "esq")
            (cons (verifica-sucessor (first lista-estados) "baixo")
+           (cons (verifica-sucessor (first lista-estados) "dir")
+           (cons (verifica-sucessor (first lista-estados) "cima")
            (retorna-sucessores-lista-estados (rest lista-estados))))))]
+    ;; Senão, retorna a lista de sucessores do resto dos estados
     [else (retorna-sucessores-lista-estados (rest lista-estados))]))
+
+(check-expect (retorna-sucessores-lista-estados LISTA-ESTADOS-1)
+              (list
+               '()
+               (make-estado 3 1 2 0 4 5 6 7 8)
+               (make-estado 1 0 2 3 4 5 6 7 8)
+               '()
+               (make-estado 1 4 2 0 3 5 6 7 8)
+               (make-estado 1 4 2 3 7 5 6 0 8)
+               (make-estado 1 4 2 3 5 0 6 7 8)
+               (make-estado 1 0 2 3 4 5 6 7 8)))
+
+(check-expect (retorna-sucessores-lista-estados LISTA-ESTADOS-2)
+              (list
+               '()
+               '()
+               (make-estado 4 5 8 1 1 2 7 0 1)
+               (make-estado 4 5 8 0 1 2 1 7 1)
+               (make-estado 1 4 2 0 3 5 6 7 8)
+               (make-estado 1 4 2 3 7 5 6 0 8)
+               (make-estado 1 4 2 3 5 0 6 7 8)
+               (make-estado 1 0 2 3 4 5 6 7 8)))
+
+;; retorna-sucessores-estados-nao-vazios : ListasDeEstados -> ListasDeEstados
+;; Objetivo : dado uma ListasDeEstados, retorna uma nova ListasDeEstados montada recursivamente
+;; removendo os valores que forem vazios, para complementar o retorno da função retorna-sucessores-lista-estados
+;; de forma separada e mais limpa
+
+;; Exemplos:
+;; (retorna-sucessores-estados-nao-vazios (retorna-sucessores-lista-estados LISTA-ESTADOS-1)) =
+;; (list
+;;  (make-estado 3 1 2 0 4 5 6 7 8)
+;;  (make-estado 1 0 2 3 4 5 6 7 8)
+;;  (make-estado 1 4 2 0 3 5 6 7 8)
+;;  (make-estado 1 4 2 3 7 5 6 0 8)
+;;  (make-estado 1 4 2 3 5 0 6 7 8)
+;;  (make-estado 1 0 2 3 4 5 6 7 8))
+
+;; (retorna-sucessores-estados-nao-vazios (retorna-sucessores-lista-estados LISTA-ESTADOS-2)) =
+;; (list
+;;  (make-estado 4 5 8 1 1 2 7 0 1)
+;;  (make-estado 4 5 8 0 1 2 1 7 1)
+;;  (make-estado 1 4 2 0 3 5 6 7 8)
+;;  (make-estado 1 4 2 3 7 5 6 0 8)
+;;  (make-estado 1 4 2 3 5 0 6 7 8)
+;;  (make-estado 1 0 2 3 4 5 6 7 8))
 
 (define (retorna-sucessores-estados-nao-vazios lista-estados)
   (cond
+    ;; Se a lista-estados estiver vazia, retorna vazio
     [(empty? lista-estados) empty]
+    ;; Se o primeiro elemento da lista não for vazio, retorna ele montado junto da lista do resto
+    ;; de elementos não vazios
     [(not (empty? (first lista-estados)))
      (cons (first lista-estados) (retorna-sucessores-estados-nao-vazios (rest lista-estados)))]
+    ;; Senão, retorna o resto dos elementos não vazios
     [else (retorna-sucessores-estados-nao-vazios (rest lista-estados))]))
 
+(check-expect (retorna-sucessores-estados-nao-vazios (retorna-sucessores-lista-estados LISTA-ESTADOS-1))
+              (list
+               (make-estado 3 1 2 0 4 5 6 7 8)
+               (make-estado 1 0 2 3 4 5 6 7 8)
+               (make-estado 1 4 2 0 3 5 6 7 8)
+               (make-estado 1 4 2 3 7 5 6 0 8)
+               (make-estado 1 4 2 3 5 0 6 7 8)
+               (make-estado 1 0 2 3 4 5 6 7 8)))
+
+(check-expect (retorna-sucessores-estados-nao-vazios (retorna-sucessores-lista-estados LISTA-ESTADOS-2))
+              (list
+               (make-estado 4 5 8 1 1 2 7 0 1)
+               (make-estado 4 5 8 0 1 2 1 7 1)
+               (make-estado 1 4 2 0 3 5 6 7 8)
+               (make-estado 1 4 2 3 7 5 6 0 8)
+               (make-estado 1 4 2 3 5 0 6 7 8)
+               (make-estado 1 0 2 3 4 5 6 7 8)))
+
+;; gera-sucessores-lista-repetidos : ListasDeEstados -> ListasDeEstados
+;; Objetivo : dada uma ListasDeEstados, deve devolver uma ListasDeEstados contendo os
+;; sucessores possíveis de serem gerados com um movimento do blank de cada estado.
+;; A função utiliza as funções retorna-sucessores-lista-estados bem como
+;; retorna-sucessores-estados-nao-vazios como auxiliares
+
+;; Exemplos:
+;; (gera-sucessores-lista LISTA-ESTADOS-1) =
+;; (list
+;;  (make-estado 3 1 2 0 4 5 6 7 8)
+;;  (make-estado 1 0 2 3 4 5 6 7 8)
+;;  (make-estado 1 4 2 0 3 5 6 7 8)
+;;  (make-estado 1 4 2 3 7 5 6 0 8)
+;;  (make-estado 1 4 2 3 5 0 6 7 8)
+;;  (make-estado 1 0 2 3 4 5 6 7 8))
+
+;; (gera-sucessores-lista LISTA-ESTADOS-2) =
+;; (list
+;;  (make-estado 4 5 8 1 1 2 7 0 1)
+;;  (make-estado 4 5 8 0 1 2 1 7 1)
+;;  (make-estado 1 4 2 0 3 5 6 7 8)
+;;  (make-estado 1 4 2 3 7 5 6 0 8)
+;;  (make-estado 1 4 2 3 5 0 6 7 8)
+;;  (make-estado 1 0 2 3 4 5 6 7 8))
+
+(define (gera-sucessores-lista-repetidos lista-estados)
+  (retorna-sucessores-estados-nao-vazios (retorna-sucessores-lista-estados lista-estados)))
+
+(check-expect (gera-sucessores-lista-repetidos LISTA-ESTADOS-1)
+              (list
+               (make-estado 3 1 2 0 4 5 6 7 8)
+               (make-estado 1 0 2 3 4 5 6 7 8)
+               (make-estado 1 4 2 0 3 5 6 7 8)
+               (make-estado 1 4 2 3 7 5 6 0 8)
+               (make-estado 1 4 2 3 5 0 6 7 8)
+               (make-estado 1 0 2 3 4 5 6 7 8)))
+
+(check-expect (gera-sucessores-lista-repetidos LISTA-ESTADOS-2)
+              (list
+               (make-estado 4 5 8 1 1 2 7 0 1)
+               (make-estado 4 5 8 0 1 2 1 7 1)
+               (make-estado 1 4 2 0 3 5 6 7 8)
+               (make-estado 1 4 2 3 7 5 6 0 8)
+               (make-estado 1 4 2 3 5 0 6 7 8)
+               (make-estado 1 0 2 3 4 5 6 7 8)))
+
 ;; gera-sucessores-lista : ListasDeEstados -> ListasDeEstados
-;; Obj : dada uma ListasDeEstados, deve devolver uma ListasDeEstados contendo os
+;; Objetivo : dada uma ListasDeEstados, deve devolver uma ListasDeEstados contendo os
 ;; sucessores possíveis de serem gerados com um movimento do blank de cada estado,
-;; sem contar sucessores repetidos
+;; sem contar sucessores repetidos. A função utiliza a função gera-sucessores-lista-repetidos
+;; como auxiliar na chamada para remover os sucessores repetidos
+
+;; Exemplos:
+;; (gera-sucessores-lista LISTA-ESTADOS-1) =
+;; (list
+;;  (make-estado 3 1 2 0 4 5 6 7 8)
+;;  (make-estado 1 4 2 0 3 5 6 7 8)
+;;  (make-estado 1 4 2 3 7 5 6 0 8)
+;;  (make-estado 1 4 2 3 5 0 6 7 8)
+;;  (make-estado 1 0 2 3 4 5 6 7 8))
+
+;; (gera-sucessores-lista LISTA-ESTADOS-2) =
+;; (list
+;;  (make-estado 4 5 8 1 1 2 7 0 1)
+;;  (make-estado 4 5 8 0 1 2 1 7 1)
+;;  (make-estado 1 4 2 0 3 5 6 7 8)
+;;  (make-estado 1 4 2 3 7 5 6 0 8)
+;;  (make-estado 1 4 2 3 5 0 6 7 8)
+;;  (make-estado 1 0 2 3 4 5 6 7 8))
+
 (define (gera-sucessores-lista lista-estados)
   (cond
-    ;; se a lista lista-estados estiver vazia, então devolver uma imagem vazia
+    ;; Se a lista lista-estados estiver vazia, então devolver vazio
     [(empty? lista-estados) empty]
-    ;; se o primeiro elemento da lista não se encontra no restante dela,
-    ;; então desenhar os sucessores dele (usando a função gera-sucessores) ao lado
-    ;; do restante dos sucessores únicos da lista
-    [(not
-      (estado-está-em?
-       (first (retorna-sucessores-estados-nao-vazios (retorna-sucessores-lista-estados lista-estados)))
-       (rest (retorna-sucessores-estados-nao-vazios (retorna-sucessores-lista-estados lista-estados)))))
-     (cons
-      (first (retorna-sucessores-estados-nao-vazios (retorna-sucessores-lista-estados lista-estados)))
-      (gera-sucessores-lista (rest (retorna-sucessores-estados-nao-vazios (retorna-sucessores-lista-estados lista-estados)))))]
-    ;; senão, retornar o restante da lista de sucessores
-    [else (gera-sucessores-lista (rest (retorna-sucessores-estados-nao-vazios (retorna-sucessores-lista-estados lista-estados))))]))
+    ;; Se o primeiro elemento da lista não se encontra no restante dela,
+    ;; então retornar o mesmo e montar junto do resto da lista
+    [(not (estado-está-em? (first lista-estados) (rest lista-estados)))
+     (cons (first lista-estados) (gera-sucessores-lista (rest lista-estados)))]
+    ;; Senão, retornar o restante da lista
+    [else (gera-sucessores-lista (rest lista-estados))]))
+
+(check-expect (gera-sucessores-lista (gera-sucessores-lista-repetidos LISTA-ESTADOS-1))
+              (list
+               (make-estado 3 1 2 0 4 5 6 7 8)
+               (make-estado 1 4 2 0 3 5 6 7 8)
+               (make-estado 1 4 2 3 7 5 6 0 8)
+               (make-estado 1 4 2 3 5 0 6 7 8)
+               (make-estado 1 0 2 3 4 5 6 7 8)))
+
+(check-expect (gera-sucessores-lista (gera-sucessores-lista-repetidos LISTA-ESTADOS-2))
+              (list
+               (make-estado 4 5 8 1 1 2 7 0 1)
+               (make-estado 4 5 8 0 1 2 1 7 1)
+               (make-estado 1 4 2 0 3 5 6 7 8)
+               (make-estado 1 4 2 3 7 5 6 0 8)
+               (make-estado 1 4 2 3 5 0 6 7 8)
+               (make-estado 1 0 2 3 4 5 6 7 8)))
 
 
 ;; ========================================================================
